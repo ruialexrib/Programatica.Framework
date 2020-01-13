53e674355a33e7624d3823e62f62a928db7d6c96
Rui Ribeiro <ruialexrib@gmail.com>
2020-01-13 20:21:57 +0000

    added comments to CompilerServices attributes

--- Programatica.Framework.Core/BaseObject.cs
+++ Programatica.Framework.Core/BaseObject.cs
@@ -21,16 +21,26 @@ namespace Programatica.Framework.Core
             InstanceDateTime = DateTime.UtcNow;
         }
 
+        /// <summary>
+        /// Allows you to obtain the method or property name of the caller to the method.
+        /// </summary>
         public string GetCallerMemberName([CallerMemberName] string callingMember = null)
         {
             return callingMember;
         }
 
+        /// <summary>
+        /// Allows you to obtain the full path of the source file that contains the caller.
+        /// This is the file path at the time of compile.
+        /// </summary>
         public string GetCallerFilePath([CallerFilePath] string callingFile = null)
         {
             return callingFile;
         }
 
+        /// <summary>
+        /// Allows you to obtain the line number in the source file at which the method is called.
+        /// </summary>
         public int GetCallerLineNumber([CallerLineNumber] int callingLineNum = 0)
         {
             return callingLineNum;
