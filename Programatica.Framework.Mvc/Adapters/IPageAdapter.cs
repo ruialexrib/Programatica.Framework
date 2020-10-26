1cba8e28050f58a10b51bb6080252d12c0bf8a5c
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-26 10:21:37 +0000

    added IPageAdapter

---
+++ Programatica.Framework.Mvc/Adapters/IPageAdapter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Programatica.Framework.Mvc.Adapters
+{
+    public interface IPageAdapter
+    {
+        string ConnectionString { get; }
+
+        List<string> PageMessages { get; }
+        List<string> PageWarnings { get; }
+        List<string> PageAlerts { get; }
+
+        string ControllerName { get; }
+        string ActionName { get; }
+    }
+}
