c763ccfc16e1e8e3837ef304c362db77df607b42
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-26 11:19:11 +0000

    added Programatica.Framework.Core.Extensions

---
+++ Programatica.Framework.Core.Extensions/ListExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Programatica.Framework.Core.Extensions
+{
+    public static class ListExtensions
+    {
+        public static List<T> OrEmptyIfNull<T>(this List<T> source)
+        {
+            return source ?? new List<T>();
+        }
+    }
+}
