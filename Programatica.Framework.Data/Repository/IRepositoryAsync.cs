6760441e2be777a753826de0b8030b976883a435
Rui Ribeiro <ruialexrib@gmail.com>
2020-10-30 23:48:57 +0000

    added RepositoryAsync

---
+++ Programatica.Framework.Data/Repository/IRepositoryAsync.cs
@@ -0,0 +1,18 @@
+﻿using Programatica.Framework.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programatica.Framework.Data.Repository
+{
+    interface IRepositoryAsync<T> : IDisposable
+        where T : IModel
+    {
+        Task<T> GetDataAsync(int id);
+        Task<IReadOnlyList<T>> GetDataAsync();
+        Task<T> InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+    }
+}
---
+++ Programatica.Framework.Data/Repository/RepositoryAsync.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Programatica.Framework.Data.Context;
+using Programatica.Framework.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Programatica.Framework.Data.Repository
+{
+    public class RepositoryAsync<T> : IRepositoryAsync<T>, IDisposable
+        where T : class, IModel
+    {
+        private readonly IDbContext _context;
+        private bool _disposed = false;
+
+        public RepositoryAsync(IDbContext context)
+        {
+            _context = context;
+        }
+
+        public virtual async Task<T> GetDataAsync(int id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> GetDataAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> InsertAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            return entity;
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            return Task.CompletedTask;
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            _disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
