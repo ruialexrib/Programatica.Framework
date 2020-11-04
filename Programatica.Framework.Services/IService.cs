5ce0f0588fe9924a3e8cb06307b14f10e331a190
Rui Ribeiro <ruialexrib@gmail.com>
2020-11-04 17:18:33 +0000

    added async methods to IService

--- Programatica.Framework.Services/IService.cs
+++ Programatica.Framework.Services/IService.cs
@@ -1,19 +1,33 @@
-﻿using Programatica.Framework.Core;
-using Programatica.Framework.Data.Models;
+﻿using Programatica.Framework.Data.Models;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Programatica.Framework.Services
 {
     public interface IService<T> : IDisposable
         where T : IModel
     {
+        Task<T> CreateAsync(T model);
         T Create(T model);
+
+        Task<T> ModifyAsync(T model);
         T Modify(T model);
+
+        Task DestroyAsync(int id);
         void Destroy(int id);
+
+        Task DeleteAsync(int id);
         void Delete(int id);
+
+        Task<T> GetAsync(int id);
         T Get(int id);
+
+        Task<IEnumerable<T>> GetAsync();
         IQueryable<T> Get();
+
+        Task<T> InspectAsync(int id);
         T Inspect(int id);
     }
 }
--- Programatica.Framework.Services/Service.cs
+++ Programatica.Framework.Services/Service.cs
@@ -2,7 +2,9 @@
 using Programatica.Framework.Services.Handlers;
 using Programatica.Framework.Services.Injector;
 using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Programatica.Framework.Services
 {
@@ -17,6 +19,37 @@ namespace Programatica.Framework.Services
         }
 
         #region IService<T>
+
+
+        public async Task<T> CreateAsync(T model)
+        {
+            try
+            {
+                model.CreatedDate = _injector.DateTimeAdapter.UtcNow;
+                model.CreatedUser = _injector.AuthUserAdapter.Name;
+
+                // handle before events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnBeforeCreating(model);
+                }
+
+                await _injector.TRepository.InsertAsync(model);
+
+                // handle after events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnAfterCreated(model);
+                }
+
+                return model;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
 
         public virtual T Create(T model)
         {
@@ -46,6 +79,35 @@ namespace Programatica.Framework.Services
                 throw;
             }
 
+        }
+
+        public async Task<T> ModifyAsync(T model)
+        {
+            try
+            {
+                model.LastModifiedDate = _injector.DateTimeAdapter.UtcNow;
+                model.LastModifiedUser = _injector.AuthUserAdapter.Name;
+
+                // handle before events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnBeforeModifying(model);
+                }
+
+                await _injector.TRepository.UpdateAsync(model);
+
+                // handle after events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnAfterModified(model);
+                }
+
+                return model;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public virtual T Modify(T model)
@@ -78,6 +140,32 @@ namespace Programatica.Framework.Services
 
         }
 
+        public async Task DeleteAsync(int id)
+        {
+            try
+            {
+                T record = await _injector.TRepository.GetDataAsync(id);
+
+                // handle before events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnBeforeDeleting(record);
+                }
+
+                await _injector.TRepository.DeleteAsync(record);
+
+                // handle after events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnAfterDeleted(record);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public virtual void Delete(int id)
         {
             try
@@ -96,6 +184,36 @@ namespace Programatica.Framework.Services
                 foreach (IEventHandler<T> handler in _injector.EventHandlers)
                 {
                     handler.OnAfterDeleted(record);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public async Task DestroyAsync(int id)
+        {
+            try
+            {
+                T record = await _injector.TRepository.GetDataAsync(id);
+
+                record.LastDestroyedDate = _injector.DateTimeAdapter.UtcNow;
+                record.LastDestroyedUser = _injector.AuthUserAdapter.Name;
+                record.IsDestroyed = true;
+
+                // handle before events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnBeforeDestroying(record);
+                }
+
+                await _injector.TRepository.UpdateAsync(record);
+
+                // handle after events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnAfterDestroyed(record);
                 }
             }
             catch (Exception)
@@ -134,6 +252,19 @@ namespace Programatica.Framework.Services
             }
         }
 
+        public async Task<T> GetAsync(int id)
+        {
+            try
+            {
+                var result = await _injector.TRepository.GetDataAsync(id);
+                return result;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public virtual T Get(int id)
         {
             try
@@ -147,11 +278,44 @@ namespace Programatica.Framework.Services
             }
         }
 
+        public async Task<IEnumerable<T>> GetAsync()
+        {
+            try
+            {
+                return await _injector.TRepository.GetDataAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
         public virtual IQueryable<T> Get()
         {
             try
             {
                 return _injector.TRepository.GetData();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+
+        public async Task<T> InspectAsync(int id)
+        {
+            try
+            {
+                var result = await _injector.TRepository.GetDataAsync(id);
+
+                // handle before events  
+                foreach (IEventHandler<T> handler in _injector.EventHandlers)
+                {
+                    handler.OnBeforeInspecting(result);
+                }
+
+                return result;
             }
             catch (Exception)
             {
