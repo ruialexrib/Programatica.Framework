bf0436ce370704b5f8fc5fb64fffc0d5f6b15c45
Rui Ribeiro <ruialexrib@gmail.com>
2021-06-11 16:33:20 +0100

    Added ElapsedTime to Base Controller v2.0.10 package released to nuget

--- Programatica.Framework.Mvc/Controllers/BaseController.cs
+++ Programatica.Framework.Mvc/Controllers/BaseController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Programatica.Framework.Mvc.Controllers
 {
@@ -10,11 +12,19 @@ namespace Programatica.Framework.Mvc.Controllers
         public List<string> PageWarnings { get; set; }
         public List<string> PageAlerts { get; set; }
 
+        private DateTime StartTime { get; set; }
+
         public BaseController()
         {
             PageMessages = new List<string>();
             PageWarnings = new List<string>();
             PageAlerts = new List<string>();
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext ctx)
+        {
+            base.OnActionExecuting(ctx);
+            StartTime = DateTime.Now;
         }
 
         public override void OnActionExecuted(ActionExecutedContext context)
@@ -28,6 +38,8 @@ namespace Programatica.Framework.Mvc.Controllers
             TempData["ControllerName"] = context.RouteData.Values["controller"].ToString();
             TempData["ControllerAction"] = context.RouteData.Values["action"].ToString();
 
+            var mls = DateTime.Now - this.StartTime;
+            TempData["ElapsedTime"] = mls.Milliseconds.ToString();
         }
 
     }
