a205c1322e4d2c3017675509e3826ab7b4268fb2
Rui Ribeiro <ruialexrib@gmail.com>
2021-08-09 14:43:38 +0100

    added more fields to ClaimBasedAuthAdapterOptions

--- Programatica.Framework.Mvc/Options/ClaimBasedAuthAdapterOptions.cs
+++ Programatica.Framework.Mvc/Options/ClaimBasedAuthAdapterOptions.cs
@@ -5,5 +5,7 @@
         public string UserNameFieldName{ get; set; }
         public string PasswordFieldName { get; set; }
         public string LastLoginDateTimeFieldName { get; set; }
+        public string UserRoleFieldName { get; set; }
+        public string UserRolePermissionFieldName { get; set; }
     }
 }
